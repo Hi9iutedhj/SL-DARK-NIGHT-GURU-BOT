0b8e17562818e4ea58bbd492bd8a5ad706803b40
Hi9iutedhj <119156201+Hi9iutedhj@users.noreply.github.com>
2023-07-16 11:15:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['447710173736', 'Owner', true],
-  ['447710173736', 'Ash', true], 
+  ['94720537393', 'Owner', true],
+  ['94720537393', 'sl max', true], 
   [''] 
 ] //Number of owners
 
